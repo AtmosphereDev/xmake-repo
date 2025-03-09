c50ba7ec41d805eff956ca08086eb2ee36f3cd98
VinkyV <75137786+VinkyV@users.noreply.github.com>
2025-03-10 02:31:18 +0300

    Update xmake.lua
    Use capstone for multiplatform and force to be static!

--- packages/f/funchook/xmake.lua
+++ packages/f/funchook/xmake.lua
@@ -10,10 +10,11 @@ package("funchook")
     on_install(function (package)
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        
+        table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
         table.insert(configs, "-DFUNCHOOK_BUILD_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DFUNCHOOK_BUILD_TESTS=OFF")
-        table.insert(configs, "-DFUNCHOOK_INSTALL=OFF")
+        table.insert(configs, "-DFUNCHOOK_DISASM=capstone")
+        table.insert(configs, "-DFUNCHOOK_BUILD_SHARED=OFF")
+        table.insert(configs, "-DFUNCHOOK_BUILD_STATIC=ON")
         
         import("package.tools.cmake").install(package, configs)
         os.mv("include/*", package:installdir("include"))
